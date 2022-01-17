82428b01429be68a357e63097cec3dd95898c699
Dominic Sykes <65317380+DominicSykes@users.noreply.github.com>
2022-01-17 13:25:48 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,86 @@
+﻿<!DOCTYPE html>
+<!--Navigation-->
+<html>
+<head>
+    <link rel="stylesheet" href="./CSS/Style.css">
+</head>
+<header>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+    <head>
+
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="About.html" target="_blank">About Me</a>
+            <a href="Portfolio.html" target="_blank">Portfolio</a>
+            <a href="mailto:dominicsykedelics@gmail.com" style="color:white">Contact</a>
+        </div>
+        </ul>
+        <span style="float:right; font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+    </head>
+
+</header>
+<body>
+    <img src="./Images/DOMtek.jpg" height="100px" width="100px" /> <br />
+    <a href="Index.html" target="_blank" style="color:black">Dominic.Sykes</a>
+
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+    </script>
+    <body>
+
+        <center>
+            <h1>Index</h1>
+        </center>
+        <h1>Who are you?</h1>
+         
+            <?php I am Mr. Sykes, I am a computer technician. ?> <br />
+            <button><a href="About.html" style="color:black">Learn More!</a></button>
+        
+        <img src="./Images/Me.jpg" height="200 px" width="200px" alt="Me.jpg" />
+        <center>
+            <h1><a href="https://www.fieldengineer.com/skills/what-is-a-pc-technician" style="color:black">Computer Technician</a></h1>
+            <!-- <p>By choice and by trade.</p>-->
+            <img src="./Images/Computer.jpg" height="200px" width="350px" />
+        </center>
+
+        <u><h1 align="Right">Web Developer</h1></u>
+        <!-- <p align="right">By Choice</p>-->
+        <img src="./Images/web.jpg" height="200px" width="350px" align="right" /> <br /><br /><br /><br />
+        <br /><br /><br /><br /><?php
+        I am working to complete my Computer Technician Diploma/Certification in 2022. I am also working on finding a job
+        and a small local apartment out in Toronto Canada. Life is not always as it seems growing up in Jamaica. ?>
+        <br /><br /><br /><br />
+        <a href="./Resume-Dominic-Sykes.pdf">My Resume`</a>
+        <footer>
+            <br /><b>
+    <br /><br /> <br /><br /><br />
+    <center>  
+            _______________________________________________________________________________________________________________________________________________ 
+    </center>
+</b>
+            <br />
+            <br />        <center>
+                <!--Socials-->
+                <a href="https://www.facebook.com/Dominic-Sykes-101350095333147" target="_blank">
+            <img src="./Images/Facebook.jpg" alt="Facebook" style="width:42px;height:42px;"></a>
+                <a href="https://www.Twitter.com/Dominic_Sykes1" target="_blank">
+
+            <img src="./Images/Twitter.jpg" alt="Twitter" style="width:42px;height:42px;"></a>
+                <a href="https://www.Instagram.com/Dominic_Sykes1" target="_blank">
+
+            <img src="./Images/Instagram.jpg" alt="Instagram" style="width:42px;height:42px;"></a>
+                <a href="https://www.Github.com/DominicSykes" target="_blank"> 
+
+            <img src="./Images/Github.jpg" alt="Facebook.jpg" style="width:42px;height:42px;"></a>
+                <a href="https://www.youtube.com/channel/UCWKOEpPyqZIKFZAdF2IpwPw"
+                   target="_blank"><img src="./Images/Youtube.jpg" alt="Youtube" style="width:42px;height:42px;"></a>
+        </footer> </center>
+        <!--I put the social links up at the top because When I tried putting them at the bottom they interfere
+    with the other texts-->
+</html>
